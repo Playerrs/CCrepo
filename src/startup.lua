a7d7bf90f52a0d315bf7113767cd4891ad541f15
Player_rs <josueplayerrs@gmail.com>
2023-01-11 21:15:39 -0300

    tirei o codigo antigo de dentro do startup.lua e botei no primeira versao.lua

--- src/startup.lua
+++ src/startup.lua
@@ -2,27 +2,34 @@
 
 
 
+
+-- Utils
+
+local rootDir = ...
+if rootDir == nil then
+    rootDir = ""
+end
+
+local function serialize(data, name)
+    if not fs.exists(rootDir..'data') then
+        fs.makeDir(rootDir..'data')
+    end
+    local f = fs.open(rootDir..'data/'..name, 'w')
+    f.write(textutils.serialize(data))
+    f.close()
+end
+
+local function unserialize(name)
+    local data
+    if fs.exists(name) then
+        local f = fs.open(name, 'r')
+        data = textutils.unserialize(f.readAll())
+        f.close()
+    end
+    return data
+end
+
+
+-- Loaders
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
-
-local again = true
-
-local li = {
-    "https://drive.google.com/u/0/uc?id=1uKuRgIOe07ngdMuznXZBbqPRbdLaZ7Gf&export=download",
-    "https://drive.google.com/u/0/uc?id=1LPQhHAwzd_Nz1_PEnSK0HY0cxhJMDu5p&export=download",
-    "https://drive.google.com/u/0/uc?id=1ONWankUbuqFDeoGkVgMnj-q6PCfsfk0N&export=download"
-}
-while again == true do
-
-    print("Reproduzindo playlist")
-
-    for i = 1, #li do
-        print(li[i])
-        shell.run("speaker play "..li[i])
-    end
-
-    write("Play another song? (y/n) ")
-    again = read()
-    if again == "n" then again = false
-    else again = true end
-end
