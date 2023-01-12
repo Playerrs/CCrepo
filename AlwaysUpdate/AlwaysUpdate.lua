1f71cc79c597c91cc03625ec7b3a54f585dac49d
Player_rs <josueplayerrs@gmail.com>
2023-01-12 20:41:10 -0300

    V 0.4 | Idk

--- AlwaysUpdate/AlwaysUpdate.lua
+++ AlwaysUpdate/AlwaysUpdate.lua
@@ -1,17 +1,25 @@
 -- Updater
 local args = {...}
-local item = args[1]
-local name = args[2]
 
-local github = "https://raw.githubusercontent.com/"
-local user = "Playerrs/"
+if args[1] == "dw" then
+    local item = args[2]
+    local name = args[3]
 
-local repos = {"CC-projects/", "CC-Music-Player/"}
-local branches = {"main/", "master/"}
+    local github = "https://raw.githubusercontent.com/"
+    local user = "Playerrs/"
 
-for _, repo in ipairs(repos) do
-    for _, branch in ipairs(branches) do
-        local url = github..user..repo..branch..item
-        print(shell.run("wget "..url.." "..name))
+    local repos = {"CC-projects/", "CC-Music-Player/"}
+    local branches = {"main/", "master/"}
+
+    for _, repo in ipairs(repos) do
+        for _, branch in ipairs(branches) do
+            local url = github..user..repo..branch..item
+            print(shell.run("wget "..url.." "..name))
+        end
     end
+
+else
+    local item = args[2]
+    local name = args[3]
+    print(shell.run("wget "..item.." "..name))
 end
