456be7e69b3d2e243ca9af88b61c79fe6eab922a
Player_rs <josueplayerrs@gmail.com>
2023-10-28 21:30:34 -0300

    V 0.49 | Just appears the actual values Better

--- UniversalRemoteConsole/configEdit.lua
+++ UniversalRemoteConsole/configEdit.lua
@@ -1,7 +1,7 @@
 -- Created by Player_rs
--- V: 0.48
+-- V: 0.49
 
-local version = 0.48
+local version = 0.49
 -- Langs
 local lang = {}
 
@@ -65,6 +65,13 @@ if editableType == "instances" then
 end
 
 -- Functions
+local function writeActualValue(value)
+    local _color = term.getTextColor()
+    term.setTextColor(colors.green)
+    print(string.format(">> %s <<", value))
+    term.setTextColor(_color)
+end
+
 local function findInstance(instanceName)
     for i = 1, #loadedInstances do
         if loadedInstances[i].name == instanceName then
@@ -75,10 +82,11 @@ local function findInstance(instanceName)
 end
 
 -- USER
-utils.printMultiLines(LANG.tip, 2)
+utils.printMultiLines(LANG.tip, 2, colors.yellow)
 
 if editableType == "instances" then
-    print("\n\n"..string.format(">> [%s] <<", instance.name))
+    print('\n\n')
+    writeActualValue(instance.name)
     print(LANG.instances.changeName)
     term.write(">> ")
     local _instanceName = read()
@@ -90,7 +98,7 @@ if editableType == "instances" then
     end
     print()
 
-    print(string.format(">> [%s] <<", instance.channel))
+    writeActualValue(instance.channel)
     print(LANG.instances.changeChannel)
     term.write(">> ")
     local _instanceChannel = read()
